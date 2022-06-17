da1e04090cbf7753907a68d3163d8640205365c5
Akhil KP <98999402+akhilkpxcoder@users.noreply.github.com>
2022-06-17 18:41:00 +0530

    view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  <vue-suggestion
+    :items="items"
+    v-model="item"
+    :setLabel="setLabel"
+    @changed="inputChange"
+    @selected="itemSelected"
+  >
+  </vue-suggestion>
+  <div>
+   <li v-for="item in this.items" :key="item">{{item}}</li>
+  </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      item: {},
+      items: [],
+    };
+  },
+  methods: {
+    itemSelected(item) {
+      this.item = item;
+    },
+    Itemlist(list){
+      console.log(list);
+     let i =0;
+     while(i<list.length)
+     {
+          console.log(list[i].Name);
+      this.items.push(list[i].Name);
+      
+      i++;
+    
+   
+  
+     }
+      console.log(this.items);
+    },
+    setLabel(item) {
+      return item.name;
+    },
+    inputChange(text) {
+      this.items=[];
+      axios.defaults.headers.common['Content-Type'] = 'application/json';
+      axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+      axios
+        .get(`http://localhost:3001/stock/${text}`)
+        .then((response) => (this.Itemlist(response.data)));
+      
+      // your search method
+      //this.items = this.items.filter((item) => item.name.indexOf(text) > -1);
+      // now `items` will be showed in the suggestion list
+    }, 
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSuggestion from 'vue-suggestion';
+
 
 Vue.config.productionTip = false
-
+Vue.use(VueSuggestion);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
